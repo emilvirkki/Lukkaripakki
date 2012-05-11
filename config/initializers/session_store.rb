7478ff468b30a7079b45b131516a010aaab7237d
Emil Virkki <emil.virkki@gmail.com>
2012-05-11 20:06:49 +0300

    * Added host to production db config * Changed some config files to use ruby 1.8.7 syntax for compatibility

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-SongArchive::Application.config.session_store :cookie_store, key: '_SongArchive_session'
+SongArchive::Application.config.session_store :cookie_store, :key => '_SongArchive_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
