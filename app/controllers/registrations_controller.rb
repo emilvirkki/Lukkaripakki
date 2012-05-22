64af5bf2e5f5428c1a6910abc09cb88c7e05207c
Emil Virkki <emil.virkki@gmail.com>
2012-05-22 22:01:04 +0300

    * Re-fixed confirmation message bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
+    flash[:notice] = t('devise.registrations.signed_up_but_unconfirmed')
     NavItem.default_page
   end
 end
